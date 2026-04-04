5294d9e026e7ad506fc64989b89b959aeca5e70b
furwadiallone-jpg <furwadiallone@gmail.com>
2026-04-04 10:54:46 +0700

    Log server start message with port number

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 80
+/* STATIC */
+app.use(express.static(path.resolve(__dirname, '../public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+/* SERVER */
+app.listen(port, () => {
+console.log(`Web Online port ${port}`)
+});
