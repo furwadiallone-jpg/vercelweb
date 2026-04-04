06af16c46045013cb3844d33f04671859aa85a0b
furwadiallone-jpg <furwadiallone@gmail.com>
2026-04-04 11:32:48 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -11,6 +11,31 @@ app.get("/",(req,res) =>{
   res.sendFile(path.resolve(__dirname, '../public/home.html'))
 });
 
+app.get("/produk",(req,res)=>{
+  // Products data
+        const productsFile = [
+            {id:'file1',name:'RGX VIP V10',price:10000,img:'p10k.jpg'},
+            {id:'file2',name:'RGX VIP V11',price:20000,img:'p20k.jpg'},
+            {id:'file3',name:'RGX VIP V12',price:30000,img:'p30k.jpg'},
+            {id:'file4',name:'RGX ULTIMATE LITE',price:35000,img:'p35k.jpg'},
+            {id:'file5',name:'RGX ULTIMATE ORI',price:40000,img:'p40k.jpg'},
+            {id:'file6',name:'RGX ULTIMATE PRO',price:50000,img:'p50k.jpg'},
+            {id:'file7',name:'RGX ULTIMATE GOLD EDITION',price:70000,img:'p70k.jpg'},
+            {id:'file8',name:'RGX ULTIMATE LEGEND EDITION',price:100000,img:'p100k.jpg'},
+            {id:'file9',name:'File Pack To Apk',price:20000,img:'filepack.jpg'}
+        ];
+        const productsCheat = [
+            {id:'cheat1',name:'Cheat Ultimate Medium',price:30000,img:'c30k.jpg'},
+            {id:'cheat2',name:'Cheat Ultimate High',price:50000,img:'c50k.jpg'},
+            {id:'cheat3',name:'Cheat Ultimate Ultra',price:120000,img:'c120k.jpg'}
+        ];
+        res.send({
+          produkfile:productsFile,
+          produkcheat:productsCheat
+        })
+  
+})
+
 // Pastikan ini diletakkan di bawah semua route lainnya
 app.use((req, res) => {
   res.status(404).sendFile(path.resolve(__dirname, '../public/404.html'));
